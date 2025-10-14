5e5ba63877a72fa37876b45153292337cd4ea4e6
LuanaMaistro <luanamaistro@gmail.com>
2025-10-13 22:04:38 -0300

    'Initial'

---
+++ GerenciamentoDeVendas/Domain/Entities/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public Documento Documento { get; set; }
+        public bool Inativo { get; set; }
+        public Contato[] Contatos { get; set; }
+
+        public Endereco EnderecoPrincipal { get; set; }
+        public Endereco[] EnderecoSecundarios { get; set; }
+
+        public void Ativa()
+        {
+            Inativo = false;
+        }
+        public void Inativa()
+        {
+            Inativo = true;
+        }
+
+        public void SetEnderecoPrincipal(Endereco endereco)
+        {
+            EnderecoPrincipal = endereco;
+        }
+
+    }
+}
