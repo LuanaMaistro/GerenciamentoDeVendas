14e9172d318df47326e97c2845ce90c2e959c479
Luana Maistro <luanamaistro@gmail.com>
2026-01-28 16:01:48 -0300

    feat: criando tabelas de endereço e contato secundarias

---
+++ GerenciamentoDeVendas/Infrastructure/Migrations/20260128182948_AlterandoCamposCliente.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterandoCamposCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContatoCelular",
+                table: "Clientes",
+                type: "TEXT",
+                maxLength: 11,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContatoEmail",
+                table: "Clientes",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContatoTelefone",
+                table: "Clientes",
+                type: "TEXT",
+                maxLength: 11,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ClienteContatos",
+                columns: table => new
+                {
+                    ClienteId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Telefone = table.Column<string>(type: "TEXT", maxLength: 11, nullable: true),
+                    Celular = table.Column<string>(type: "TEXT", maxLength: 11, nullable: true),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClienteContatos", x => new { x.ClienteId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_ClienteContatos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClienteEnderecosSecundarios",
+                columns: table => new
+                {
+                    ClienteId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    CEP = table.Column<string>(type: "TEXT", maxLength: 8, nullable: false),
+                    Logradouro = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Numero = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    Complemento = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Bairro = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Cidade = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    UF = table.Column<string>(type: "TEXT", maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClienteEnderecosSecundarios", x => new { x.ClienteId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_ClienteEnderecosSecundarios_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClienteContatos");
+
+            migrationBuilder.DropTable(
+                name: "ClienteEnderecosSecundarios");
+
+            migrationBuilder.DropColumn(
+                name: "ContatoCelular",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "ContatoEmail",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "ContatoTelefone",
+                table: "Clientes");
+        }
+    }
+}
---
+++ GerenciamentoDeVendas/Infrastructure/Migrations/20260128185603_CriandoIdsTabelasSecundarias.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoIdsTabelasSecundarias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ClienteEnderecosSecundarios",
+                table: "ClienteEnderecosSecundarios");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ClienteContatos",
+                table: "ClienteContatos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ClienteEnderecosSecundarios",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ClienteContatos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ClienteEnderecosSecundarios",
+                table: "ClienteEnderecosSecundarios",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ClienteContatos",
+                table: "ClienteContatos",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClienteEnderecosSecundarios_ClienteId",
+                table: "ClienteEnderecosSecundarios",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClienteContatos_ClienteId",
+                table: "ClienteContatos",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ClienteEnderecosSecundarios",
+                table: "ClienteEnderecosSecundarios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClienteEnderecosSecundarios_ClienteId",
+                table: "ClienteEnderecosSecundarios");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ClienteContatos",
+                table: "ClienteContatos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClienteContatos_ClienteId",
+                table: "ClienteContatos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ClienteEnderecosSecundarios",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ClienteContatos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ClienteEnderecosSecundarios",
+                table: "ClienteEnderecosSecundarios",
+                columns: new[] { "ClienteId", "Id" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ClienteContatos",
+                table: "ClienteContatos",
+                columns: new[] { "ClienteId", "Id" });
+        }
+    }
+}
