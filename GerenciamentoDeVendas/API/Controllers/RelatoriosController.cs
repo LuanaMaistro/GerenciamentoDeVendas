3fdead6d71bac9ea82606aa845870c149e2b9db4
LuanaMaistro <luanamaistro@gmail.com>
2026-03-24 21:09:41 -0300

    fix: removendo AllowAnnonymous

--- GerenciamentoDeVendas/API/Controllers/RelatoriosController.cs
+++ GerenciamentoDeVendas/API/Controllers/RelatoriosController.cs
@@ -8,8 +8,6 @@ namespace API.Controllers
     [ApiController]
     [Route("api/[controller]")]
     [Authorize]
-    // TODO: remover [AllowAnonymous]
-    [AllowAnonymous]
     public class RelatoriosController : ControllerBase
     {
         private readonly IRelatorioService _relatorioService;
